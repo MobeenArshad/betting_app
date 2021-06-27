2227ac6e3a5223a617e40c106472499989741950
Waqas Aslam <smslegends@gmail.com>
2021-06-28 02:23:14 +0500

    updated design

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,22 @@
 import 'package:betting_app/views/SignIn.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 
 void main(){
-  runApp(MaterialApp(
-    title: 'Betting App',
+  runApp(NeumorphicApp(
     debugShowCheckedModeBanner: false,
+    title: 'Betting App',
+    themeMode: ThemeMode.light,
+    theme: NeumorphicThemeData(
+      baseColor: Color(0xFFFFFFFF),
+      lightSource: LightSource.topLeft,
+      depth: 10,
+    ),
+    darkTheme: NeumorphicThemeData(
+      baseColor: Color(0xFF3E3E3E),
+      lightSource: LightSource.topLeft,
+      depth: 6,
+    ),
     home: SignIn(),
   ));
 }
